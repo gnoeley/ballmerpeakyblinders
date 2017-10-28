636c4ed072d4af30e20f2cdf5617ddaba9937779
leon.pelech <leon.pelech@autotrader.co.uk>
2017-10-28 20:08:47 +0100

    replace embedded database bean with configuration

--- src/main/kotlin/com/github/gnoely/Application.kt
+++ src/main/kotlin/com/github/gnoely/Application.kt
@@ -1,30 +1,14 @@
 package com.github.gnoely
 
 import com.github.gnoely.repository.AisleRepository
-import com.github.gnoely.repository.OrderRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
-import javax.sql.DataSource
 
 
 @SpringBootApplication
 open class Application {
-
-    @Bean
-    open fun dataSource(): DataSource {
-        // no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
-        val builder = EmbeddedDatabaseBuilder()
-        return builder
-                .setType(EmbeddedDatabaseType.H2)
-                .setName("shopping")
-                .addScript("db/schema.sql")
-                .addScript("db/data.sql")
-                .build()
-    }
 
     @Bean
     open fun commandLineRunner(repo: AisleRepository): CommandLineRunner {
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,5 +1,5 @@
 create table aisle(aisle_id VARCHAR(255), aisle VARCHAR(255));
-create table order(order_id VARCHAR(255), user_id VARCHAR(255), eval_set BOOLEAN, order_number BIGINT, order_day_of_the_week BIGINT, order_hour_of_day BIGINT, days_since_prior_order BIGINT);
+create table orders(order_id VARCHAR(255), user_id VARCHAR(255), eval_set BOOLEAN, order_number BIGINT, order_day_of_the_week BIGINT, order_hour_of_day BIGINT, days_since_prior_order BIGINT);
 create table product(product_id VARCHAR(255), name VARCHAR(255), aisle_id VARCHAR(255), department_id VARCHAR(255));
 create table order_products_train(order_id VARCHAR(255), product_id VARCHAR(255), add_to_cart_order BIGINT, reordered BOOLEAN);
 create table order_products_prior(order_id VARCHAR(255), product_id VARCHAR(255), add_to_cart_order BIGINT, reordered BOOLEAN);
