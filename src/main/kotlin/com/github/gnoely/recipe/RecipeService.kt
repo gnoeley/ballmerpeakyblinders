f661444ef8ecfb952c27768dfabd63c2efd706ab
Phillip.Taylor <phillip.taylor@autotrader.co.uk>
2017-10-28 17:13:08 +0100

    Get first recipe as Recipe object

--- src/main/kotlin/com/github/gnoely/recipe/RecipeService.kt
+++ src/main/kotlin/com/github/gnoely/recipe/RecipeService.kt
@@ -1,14 +1,12 @@
 package com.github.gnoely.recipe
 
+import com.kaloer.yummly.models.Recipe
 
 
 object RecipeService {
 
-    fun getFirstRecipeForIngredient(ingredient: String) : Any {
-
-
-        return "A"
-
+    fun getFirstRecipeForIngredient(ingredient: String) : Recipe {
+        return YummlyClient.getRecipe(ingredient)
     }
 
 
--- src/main/kotlin/com/github/gnoely/recipe/YummlyClient.kt
+++ src/main/kotlin/com/github/gnoely/recipe/YummlyClient.kt
@@ -1,6 +1,8 @@
 package com.github.gnoely.recipe
 
 import com.kaloer.yummly.Yummly
+import com.kaloer.yummly.models.Recipe
+import com.kaloer.yummly.models.SearchResult
 
 object YummlyClient {
 
@@ -8,9 +10,9 @@ object YummlyClient {
     val yummly = Yummly("87489517", "bafc256f1e442c00e57753cdd653636d")
 
 
-    fun getRecipes(ingredient : String) : Any {
-
-        return yummly.search(ingredient)
+    fun getRecipe(ingredient : String) : Recipe {
+        val searchResult: SearchResult = yummly.search(ingredient)
+        return searchResult.matches[0]
     }
 
 }
