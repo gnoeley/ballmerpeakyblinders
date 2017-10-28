bfbdcea2de6bd33a7dab9e7713fd679913165bd8
Phillip.Taylor <phillip.taylor@autotrader.co.uk>
2017-10-28 16:35:42 +0100

    Created generic file reader

--- src/main/kotlin/com/github/gnoely/model/CSVModels.kt
+++ src/main/kotlin/com/github/gnoely/model/CSVModels.kt
@@ -1,9 +1,7 @@
 package com.github.gnoely.model
 
-import java.nio.file.Files
-import java.nio.file.Paths
+import com.github.gnoely.model.filereader.FileReader
 import java.util.concurrent.TimeUnit
-import java.util.stream.Collectors
 
 data class Aisle(val aisleId: Int, val asile: String)
 
@@ -24,15 +22,23 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         val start: Long = System.nanoTime()
-        val asilesFiles: List<Aisle> = Files.readAllLines(Paths.get("./data/raw/aisles.csv"))
-                .parallelStream()
-                .skip(1)
-                .map({
-                    val csv: List<String> = it.split(',')
-                    Aisle(csv[0].toInt(), csv[1])
-                })
-//                .limit(10)
-                .collect(Collectors.toList())
+
+        val aisleStrings: List<String> = FileReader.readFile("./data/raw/aisles.csv");
+        val aisles: List<Aisle> = aisleStrings.map {
+            val splitString: List<String> = it.split(',');
+            Aisle(splitString[0].toInt(), splitString[1])
+        }
+        aisles.forEach{ println(it)}
+
+//        val asilesFiles: List<Aisle> = Files.readAllLines(Paths.get("./data/raw/aisles.csv"))
+//                .parallelStream()
+//                .skip(1)
+//                .map({
+//                    val csv: List<String> = it.split(',')
+//                    Aisle(csv[0].toInt(), csv[1])
+//                })
+////                .limit(10)
+//                .collect(Collectors.toList())
 
         val end: Long = System.nanoTime()
         println("DONE: ${TimeUnit.NANOSECONDS.toMillis(end-start)}ms")
