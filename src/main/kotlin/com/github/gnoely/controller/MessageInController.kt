0825911a6b045e792e63297735d5613b8664c758
simon.dudley <simon.dudley@autotrader.co.uk>
2017-10-29 11:52:00 +0000

    /message GET and POST entry point

--- src/main/kotlin/com/github/gnoely/controller/MessageInController.kt
+++ src/main/kotlin/com/github/gnoely/controller/MessageInController.kt
@@ -3,6 +3,7 @@ package com.github.gnoely.controller
 import com.github.gnoely.model.Reply
 import com.github.gnoely.service.ReplyBuildingService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,12 +13,22 @@ class MessageInController {
 
     @Autowired lateinit var replyBuildingService: ReplyBuildingService
 
+    @GetMapping("/message")
+    @ResponseBody
+    fun sendMessage(): String {
+        val reply: Reply = replyBuildingService.buildReply("@siladu", emptyList())
+        println(reply.imageUrl)
+        println(reply.message)
+        return "{reply: ${reply.message}, image: ${reply.imageUrl}}"
+//        return "Deprecated... sorry ;-)"
+    }
+
     @PostMapping("/message")
     @ResponseBody
     fun sendMessage(message: String): String {
-//        val reply: Reply = replyBuildingService.buildReply(message)
-//        return "{reply: ${reply.message}, image: ${reply.imageUrl}}"
-        return "Deprecated... sorry ;-)"
+        val reply: Reply = replyBuildingService.buildReply(message, emptyList())
+        return "{reply: ${reply.message}, image: ${reply.imageUrl}}"
+//        return "Deprecated... sorry ;-)"
     }
 
 
