300a13e796058932f21c976e0f887785d7807b84
Phillip.Taylor <phillip.taylor@autotrader.co.uk>
2017-10-29 03:05:19 +0000

    Tweet with a picture!

--- src/main/kotlin/com/github/gnoely/twitter/TwitterOut.kt
+++ src/main/kotlin/com/github/gnoely/twitter/TwitterOut.kt
@@ -1,12 +1,15 @@
 package com.github.gnoely.twitter
 
 
+import org.apache.commons.io.FileUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 import twitter4j.StatusUpdate
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
+import java.net.URL
+import java.nio.file.Files
 import javax.annotation.PostConstruct
 
 
@@ -53,9 +56,13 @@ class TwitterOut {
 
     private fun buildMessage(inReplyToStatusId: Long, mentionUser: String, body: String, imageUrl: String?): StatusUpdate {
         val statusUpdate = StatusUpdate("@$mentionUser $body")
-//        if (imageUrl != null) {
-//            statusUpdate.media(File(imageUrl))
-//        }
+        if (imageUrl != null) {
+            val url = URL(imageUrl)
+            val file = Files.createTempFile("temp", ".tmp").toFile()
+            FileUtils.copyURLToFile(url, file)
+            statusUpdate.media(file)
+            file.deleteOnExit()
+        }
         statusUpdate.inReplyToStatusId(inReplyToStatusId)
         return statusUpdate
     }
